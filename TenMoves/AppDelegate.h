b501979dd1732404ed5c5b7847b57f8b55d8c605
David Pedersen <david.pdrsn@gmail.com>
2014-07-10 20:12:33 +0200

    Initial Commit

---
+++ TenMoves/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TenMoves
+//
+//  Created by David Pedersen on 10/07/14.
+//  Copyright (c) 2014 David Pedersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TenMoves/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TenMoves
+//
+//  Created by David Pedersen on 10/07/14.
+//  Copyright (c) 2014 David Pedersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
