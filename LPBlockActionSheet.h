ccb91f81fcd364c2e79812758b154a1e59e72d9f
David Pedersen <david.pdrsn@gmail.com>
2014-08-10 11:05:18 +0200

    Add LPBlockActionSheet

---
+++ LPBlockActionSheet.h
@@ -0,0 +1,22 @@
+//
+//  LPBlockActionSheet.h
+//  ActionSheetBlocks
+//
+//  Created by David Pedersen on 10/08/14.
+//  Copyright (c) 2014 David Pedersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPBlockActionSheet : NSObject
+
+@property (nonatomic, copy) void (^willDismissCallBack)();
+@property (nonatomic, copy) void (^willPresentCallBack)();
+
+- (void)setTitle:(NSString *)title;
+- (void)setDesctructiveButtonTitle:(NSString *)title block:(void (^)())block;
+- (void)setCancelButtonTitle:(NSString *)title block:(void (^)())block;
+- (void)addButtonWithTitle:(NSString *)title block:(void (^)())block;
+- (void)showInView:(UIView *)view;
+
+@end
