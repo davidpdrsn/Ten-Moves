6761bec2a9a8003710f3e5f0423592f589e3d4ed
David Pedersen <david.pdrsn@gmail.com>
2014-09-01 12:36:27 +0200

    Link to site and twitter
    Fixes #27

--- TenMoves/MovesTableViewController.m
+++ TenMoves/MovesTableViewController.m
@@ -20,6 +20,10 @@
 @property (strong, nonatomic) ArrayDataSource *dataSource;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *addButton;
 
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
+@property (weak, nonatomic) IBOutlet UIButton *websiteButton;
+@property (weak, nonatomic) IBOutlet UIButton *twitterButton;
+
 @end
 
 @implementation MovesTableViewController
@@ -46,6 +50,18 @@
     self.tableView.allowsSelectionDuringEditing = YES;
     
     self.tableView.delegate = self;
+    
+    self.bottomView.backgroundColor = [UIColor clearColor];
+}
+
+- (IBAction)websiteButtonTapped:(id)sender {
+    NSURL *url = [NSURL URLWithString:@"http://tenmoves.net"];
+    [[UIApplication sharedApplication] openURL:url];
+}
+
+- (IBAction)twitterButtonTapped:(id)sender {
+    NSURL *url = [NSURL URLWithString:@"http://twitter.com/tenmovesapp"];
+    [[UIApplication sharedApplication] openURL:url];
 }
 
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated {
