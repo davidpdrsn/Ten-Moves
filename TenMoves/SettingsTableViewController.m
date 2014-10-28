c0a08024829587f40e6b4f9cad693d465ab3b1f6
David Pedersen <david.pdrsn@gmail.com>
2014-10-28 12:37:18 +0100

    Add UI for sending feedback email
    In settings screen.
    
    Makes it easier for users to send me email.

--- TenMoves/SettingsTableViewController.m
+++ TenMoves/SettingsTableViewController.m
@@ -8,8 +8,9 @@
 
 #import "SettingsTableViewController.h"
 #import "Settings.h"
+#import <MessageUI/MessageUI.h>
 
-@interface SettingsTableViewController ()
+@interface SettingsTableViewController () <MFMailComposeViewControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UISwitch *shareMovesWithAPISwitch;
 @property (strong, nonatomic) Settings *settings;
@@ -34,9 +35,16 @@
 }
 
 - (void)tableView:(UITableView *)tableView didHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section != 1) return;
-    if (indexPath.row == 0) { [self openWebsite]; }
-    if (indexPath.row == 1) { [self openTwitter]; }
+    if (indexPath.section == 1) {
+        if (indexPath.row == 0) { [self openWebsite]; }
+        if (indexPath.row == 1) { [self openTwitter]; }
+    }
+    
+    if (indexPath.section == 2 && indexPath.row == 0) {
+        [self openFeedbackEmailComposer];
+    }
+    
+    [self.tableView deselectRowAtIndexPath:self.tableView.indexPathForSelectedRow animated:YES];
 }
 
 - (IBAction)openWebsiteButtonTapped:(id)sender {
@@ -49,6 +57,21 @@
 
 #pragma mark - helpers
 
+- (void)openFeedbackEmailComposer {
+    if ([MFMailComposeViewController canSendMail]) {
+        MFMailComposeViewController *mail = [[MFMailComposeViewController alloc] init];
+        mail.subject = @"Feedback for Ten Moves";
+        [mail setToRecipients:@[@"feedback@tenmoves.net"]];
+        mail.mailComposeDelegate = self;
+        mail.view.tintColor = self.view.tintColor;
+        
+        [self presentViewController:mail animated:YES completion:nil];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Cannot send email" message:nil delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
 - (void)openTwitter {
     NSURL *url = [NSURL URLWithString:@"http://twitter.com/tenmovesapp"];
     [[UIApplication sharedApplication] openURL:url];
@@ -57,6 +80,12 @@
 - (void)openWebsite {
     NSURL *url = [NSURL URLWithString:@"http://tenmoves.net"];
     [[UIApplication sharedApplication] openURL:url];
+}
+
+#pragma mark - mail composer delegate
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
